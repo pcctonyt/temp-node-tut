ff09f6b691b6eca188b906756cba3685ecb56bbf
pcctonyt <104700781+pcctonyt@users.noreply.github.com>
2022-08-14 13:02:01 -0500

    first commit

---
+++ codeblocks/block2Globals.js
@@ -0,0 +1,14 @@
+// GLOBALS - NO WINDOW !!!!
+
+// __dirname - path to current directory
+// __filename - file name
+// require - function to use modules (CommonJS)
+// module - info about current module (file)
+// process - info about environment where the program is being executed
+// setInterval - set time delays for things to happen
+//  takes in a function and a time in milliseconds
+// setTimeout
+
+setInterval(() => {
+    console.log("Hello World!")
+}, 5000)
